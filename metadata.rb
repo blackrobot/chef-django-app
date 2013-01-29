a3d3bd88be03b614274b5d3ccb2a67a5dd52fc09
Damon Jablons <damonjablons@gmail.com>
2013-01-29 17:20:31 -0500

    Fixed up dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,7 @@ description      "An opinionated deployment for Django applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-%w{ nginx python supervisor uwsgi virtualenvwrapper
-    git_ssh_wrapper database mysql postgresql }.each do |dep|
+%w{ nginx python supervisor database mysql postgresql }.each do |dep|
   depends dep
 end
 
